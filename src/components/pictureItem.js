70a6ff46f82f3d2760553313c93671f356b44aaf
Cameron Cooper <cameron.w.cooper@gmail.com>
2019-02-12 17:32:29 -0700

    added px

--- src/components/pictureItem.js
+++ src/components/pictureItem.js
@@ -4,8 +4,8 @@ const PictureItem = ({photo}) => (
     <div className="img-wrapper">
         <a download href={photo.display_url}><img src={photo.display_url} alt="change this"/></a>
         <div className="img-info">
-            <div className="item">Width: {photo.width}</div>
-            <div className="item">Height: {photo.height}</div>
+            <div className="item">Width: {photo.width}px</div>
+            <div className="item">Height: {photo.height}px</div>
             <div className="item">Source: {photo.source}</div>
         </div>
     </div>
